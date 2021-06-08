4e0f9477fb0fce65a45dbcd951ab897259a9bec0
aman2557 <80155597+aman2557@users.noreply.github.com>
2021-06-08 21:59:20 +0100

    Add files via upload

---
+++ component.h
@@ -0,0 +1,490 @@
+#include <iostream>
+#include <vector>
+#include <complex>
+#include <string>
+#include <cstdlib>
+#include <fstream>
+#include <cmath>
+#include <sstream>
+#include <iomanip>
+
+// ********************************************************************************
+// Component header to provide capability to all component classes and functions
+// ********************************************************************************
+
+// function to convert the unit multipliers
+float string_to_float(const std::string& label){
+
+    if(label.find("p") != std::string::npos){
+        return std::stof(label) * pow(10,-12);
+    }
+
+    if(label.find("n") != std::string::npos){
+        return std::stof(label) * pow(10,-9);
+    }
+
+    if((label.find("u") != std::string::npos) || (label.find("μ") != std::string::npos)){
+        return std::stof(label) * pow(10,-6);
+    }
+
+    if(label.find("m") != std::string::npos){
+        return std::stof(label) * pow(10,-3);
+    }
+
+    if(label.find("k") != std::string::npos){
+        return std::stof(label) * pow(10,3);
+    }
+
+    if(label.find("Meg") != std::string::npos){
+        return std::stof(label) * pow(10,6);
+    }
+
+    if(label.find("G") != std::string::npos){
+        return std::stof(label) * pow(10,9);
+    }
+
+    return std::stof(label);
+}
+
+int label_to_idx(const std::string& label){
+    if(label == "0"){
+        int zero = 0;
+        return zero;
+    }
+    std::string out;
+
+    for(int i = 1; i < label.size(); i++){
+        out.push_back(label[i]);
+    }
+
+    return std::stoi(out);
+}
+
+// ********************************************************************************
+// COMPONENT CLASS (general)
+// ********************************************************************************
+class Component{
+public:
+    std::complex<float> G;
+    std::complex<float> I;
+    std::complex<float> V;
+    float w;
+
+    virtual ~Component(){};
+
+    virtual std::complex<float> getI(){
+        return I;
+    }
+
+    std::string getCompName(){
+        return CompName;
+    };
+
+    virtual int getNodeA(){
+        return label_to_idx(NodeA);
+    };
+
+    virtual int getNodeB(){
+        return label_to_idx(NodeB);
+    };
+
+    virtual int getNodeC(){
+        return label_to_idx(NodeC);
+    };
+
+
+    virtual int getNodeD(){
+        return label_to_idx(NodeD);
+    };
+
+
+    virtual bool AC(std::string value){
+        char mode = value[0];
+        if(mode == 'A'){
+            return true;
+        }
+        return false;
+    }
+
+protected:
+    std::string CompName;
+    std::string NodeA;
+    std::string NodeB;
+    std::string NodeC;
+    std::string NodeD;
+    std::string value;
+};
+
+std::vector<std::string> Getinfo(std::string aline){
+    std::stringstream ss(aline); // used to split string around spaces
+    std::vector<std::string> info;
+    std::string word; // for storing each word
+
+    // while loop to traverse through all words till we get strings to store in string word
+    while (ss >> word){
+        // print the read word
+        info.push_back(word);
+    }
+    return info;
+}
+
+// AC function is in form <amplitude> <phase>
+std::complex<float> get_AC(std::string str1, std::string str2){
+    str1.erase(0,3);
+    int idx = str2.size()-1;
+    str2.erase(idx,1);
+    float A = std::stof(str1);
+    float theta = std::stof(str2);
+    theta = theta / 180 * M_PI;
+
+    std::complex<float> c;
+
+    // A*exp(j*theta) or A*(cos(theta)+isin(theta))
+    // store sine and cosine in real and imaginary parts
+    c.real(A*cos(theta));
+    c.imag(A*sin(theta));
+    return c;
+}
+
+// function to solve the magnitude of the phasor
+float get_magnitude(std::complex<float> val){
+    float re = val.real();
+    float im = val.imag();
+
+    float A = sqrt(pow(re,2) + pow(im,2));
+    return A;
+}
+
+// function to solve the phase of the phasor (theta)
+float get_phase(std::complex<float> val){
+    float re = val.real();
+    float im = val.imag();
+
+    if(re == 0){
+        if(im < 0){
+            return 270.0;
+        }
+        if(im > 0){
+            return 90.0;
+        }
+        return 0.0;
+    }
+    else if(im == 0 && re < 0){
+        return 180.0;
+    }
+
+    float theta = atan(im/re)*180 /M_PI;
+    return theta;
+}
+
+// function to ignore excess information given in the netlist file
+std::vector<std::string> newline(std::vector<std::string> line){
+    std::string ast = "*";
+    std::string c = "c";
+    std::string endFile = "end";
+
+    std::string::size_type idx;
+
+    for(int i = 0; i < line.size(); i++){
+        idx = line[i].find(ast);
+        if(idx == std::string::npos){}
+        else{
+            line.erase(line.begin() + (i));
+        }
+    }
+
+    for(int i = 0; i < line.size(); i++){
+        idx = line[i].find(c);
+        if(idx == std::string::npos){}
+        else{
+            line.erase(line.begin() + i);
+        }
+    }
+
+    for(int i = 0; i < line.size(); i++){
+        idx = line[i].find(endFile);
+        if(idx == std::string::npos){}
+        else{
+            line.erase(line.begin() + i);
+        }
+    }
+
+    return line;
+}
+
+// ********************************************************************************
+// CLASSES FOR EACH TYPE OF COMPONENT (specific)
+// ********************************************************************************
+// class for voltage source
+class Voltage_source: public Component{
+public:
+    Voltage_source(){
+        V = 0;
+    }
+
+    Voltage_source(std::vector<std::string> info){
+        CompName = info[0];
+        NodeA = info[1];
+        NodeB = info[2];
+        G = 0;
+
+        if(AC(info[3])){
+            // get the value after AC
+            V = get_AC(info[3], info[4]);
+        }
+        else{
+            V = 0;
+        }
+    }
+};
+
+// class for current source
+class Current_source: public Component{
+public:
+    Current_source(){
+        I = 0;
+    }
+
+    Current_source(std::vector<std::string> info){
+        CompName = info[0];
+        NodeA = info[1];
+        NodeB = info[2];
+        G = 0;
+
+        if(AC(info[3])){
+            //get the value after AC
+            I = get_AC(info[3], info[4]);
+        }
+        else{
+            I = 0;
+        }
+    }
+};
+
+// class for resistor
+class Resistor: public Component{
+public:
+    Resistor(){
+        G = 0;
+    }
+
+    Resistor(std::vector<std::string> info){
+        CompName = info[0];
+        NodeA = info[1];
+        NodeB = info[2];
+        if(info[3] == "INFINITE"){
+            G = 0;
+        }
+        else{
+            G.real(1/string_to_float(info[3]));
+        }
+    }
+
+};
+
+// class for capacitor
+class Capacitor: public Component{
+public:
+    Capacitor(){
+        G = 0;
+    }
+
+    Capacitor(std::vector<std::string> info ){
+        CompName = info[0];
+        NodeA = info[1];
+        NodeB = info[2];
+        G.imag(w * string_to_float(info[3]));
+    }
+};
+
+// class for inductor
+class Inductor: public Component{
+public:
+    Inductor(){
+        G = 0;
+    }
+
+    Inductor(std::vector<std::string> info){
+        CompName = info[0];
+        NodeA = info[1];
+        NodeB = info[2];
+        G.imag(-1/(w * string_to_float(info[3])));
+    }
+};
+
+// class for diode
+class Diode: public Component{
+public:
+    std::string resistor_rd;
+    std::string Id;
+
+    Diode(std::vector<std::string> info){
+        CompName = info[0];
+
+        resistor_rd.append("Rd_");
+        resistor_rd.append(info[0]);
+        resistor_rd.push_back(' ');
+        resistor_rd = resistor_rd + (info[1]);
+        resistor_rd.push_back(' ');
+        resistor_rd = resistor_rd + (info[2]);
+        resistor_rd.push_back(' ');
+        resistor_rd.append("1.6186e9"); // rd = VT/Id
+
+        Id.append("Id_");
+        Id.append(info[0]);
+        Id.push_back(' ');
+        Id = Id + (info[1]);
+        Id.push_back(' ');
+        Id = Id + (info[2]);
+        Id.push_back(' ');
+        Id.append("AC(1.5445e-11 0)"); // Id = 10^-12*(e^(0.7/0.025)-1)
+    }
+};
+
+// class for BJT
+class BJT: public Component{
+public:
+    //Q1 N003 N001 0 NPN
+
+    std::string resistor_rbe;
+    std::string resistor_r0;
+    std::string Ic;
+
+    BJT(std::vector<std::string> info){
+        CompName = info[0];
+
+        resistor_rbe.append("Rbe_");
+        resistor_rbe.append(info[0]);
+        resistor_rbe.push_back(' ');
+        resistor_rbe = resistor_rbe + (info[2]);
+        resistor_rbe.push_back(' ');
+        resistor_rbe = resistor_rbe + (info[3]);
+        resistor_rbe.push_back(' ');
+
+        resistor_r0.append("R0_");
+        resistor_r0.append(info[0]);
+        resistor_r0.push_back(' ');
+        resistor_r0 = resistor_r0 + (info[1]);
+        resistor_r0.push_back(' ');
+        resistor_r0 = resistor_r0 + (info[3]);
+        resistor_r0.push_back(' ');
+
+        Ic.append("Ic_");
+        Ic.append(info[0]);
+        Ic.push_back(' ');
+        Ic = Ic + (info[1]);
+        Ic.push_back(' ');
+        Ic = Ic + (info[3]);
+        Ic.push_back(' ');
+
+        // details for NPN (2N2222) BJT
+        if(info[4] == "NPN"){
+            // gm = Ic/VT = 800mA/25mV = 32;
+            // Rbe = beta/gm = 200/32 = 6.25;
+            // r0 = VA/Ic = 100/0.8 = 125;
+
+            resistor_rbe.append("6.25");
+            resistor_r0.append("125");
+            Ic.append("AC(0.8 0)");
+        }
+
+        // details for PNP (2N2907) BJT
+        if(info[4] == "PNP"){
+            // gm = Ic/VT = 600mA/25mV = 24;
+            // Rbe = beta/gm = 250/24 = 10.4167;
+            // r0 = VA/Ic = 120/0.6 = 200;
+
+            resistor_rbe.append("10.4167");
+            resistor_r0.append("200");
+            Ic.append("AC(0.6 0)");
+        }
+    }
+};
+
+// class for MOSFET
+class MOSFET: public Component{
+public:
+    std::string resistor_rgs;
+    std::string resistor_r0;
+    std::string Id;
+
+    MOSFET(std::vector<std::string> info){
+        CompName = info[0];
+
+        // between gate and source is open circuit with potential difference of Vgs
+        // represent as a resistor with infinite resistance;
+
+        resistor_rgs.append("Rgs_");
+        resistor_rgs.append(info[0]);
+        resistor_rgs.push_back(' ');
+        resistor_rgs = resistor_rgs + (info[2]);
+        resistor_rgs.push_back(' ');
+        resistor_rgs = resistor_rgs + (info[3]);
+        resistor_rgs.push_back(' ');
+        resistor_rgs.append("infinite");
+
+        resistor_r0.append("R0_");
+        resistor_r0.append(info[0]);
+        resistor_r0.push_back(' ');
+        resistor_r0 = resistor_r0 + (info[1]);
+        resistor_r0.push_back(' ');
+        resistor_r0 = resistor_r0 + (info[3]);
+        resistor_r0.push_back(' ');
+
+        Id.append("Id_");
+        Id.append(info[0]);
+        Id.push_back(' ');
+        Id = Id + (info[1]);
+        Id.push_back(' ');
+        Id = Id + (info[3]);
+        Id.push_back(' ');
+
+        // details for NMOS (BS170) MOSFET
+        if(info[4] == "NMOS"){
+            // gm = 2K(Vgs-Vt)
+            // r0 = VA/Id = ???;
+            // Id = gm * Vgs;
+
+            // Id = 0.2A
+            // Vt = 2V
+            // Rds = 1.2 ohm when Vgs =10V
+
+            resistor_r0.append("1.2");
+            Id.append("AC(0.2 0)");
+        }
+
+        // details for PMOS (BS250) MOSFET
+        if(info[4] == "PMOS"){
+            // Id = -0.2A (continuous drain current)
+            // rds(on) = 14 ohms when Vgs = -10V
+
+            resistor_r0.append("14");
+            Id.append("AC(0.6 0)");
+        }
+    }
+};
+
+// class for voltage controlled current source
+class V_Current_source: public Component{
+public:
+    V_Current_source(){
+        I = 0;
+    }
+
+    V_Current_source(std::vector<std::string> info){
+        CompName = info[0];
+        NodeA = info[1];
+        NodeB = info[2];
+        NodeC = info[3];
+        NodeD = info[4];
+
+        G = 0;
+
+        // write a program to obtain the voltage between node c and d
+        // need to finish this
+
+        float Vcd = 0.7; //find this value from dc or input manually
+
+        I = Vcd * string_to_float(info[5]);
+    }
+};
---
+++ main.cpp
@@ -0,0 +1,489 @@
+#include <iostream>
+#include <complex>
+#include <vector>
+#include <string>
+#include <cstdlib>
+#include <fstream>
+#include <cmath>
+#include <sstream>
+#include <iomanip>
+#include <stdexcept>
+#include <Eigen/Dense>
+
+#include "component.h"
+
+typedef std::complex<float> element;
+typedef std::vector<element> netvec;
+
+
+int main(){
+    std::cout << "------------------------------------------------------------" << std::endl;
+    std::cout << "Welcome to the AC Circuit Simulation!" << std::endl;
+    std::cout << "------------------------------------------------------------" << std::endl << std::endl;
+
+    std::string inputline;
+
+    std::vector<std::string> firstlet;
+    std::vector<std::string> line;
+    std::string filename;
+
+    std::cout << "Please enter name of the input file: ";
+    std::cin >> filename;
+
+    std::ifstream infile;
+    infile.open(filename);
+
+    if(!infile.is_open()){
+        std::cout << "Error, can not open input file." << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    while(getline(infile, inputline)){
+        line.push_back(inputline);
+    }
+
+    for(int i = 0; i < line.size(); i++){
+           std::cout << line[i] << std::endl;
+    }
+
+    std::cout << std::endl;
+
+    std::string modeline = line[line.size()-2];
+    std::vector<std::string> info2 = Getinfo(modeline);
+
+    float n_steps, f_n;
+
+    n_steps = 1;
+    f_n = 0;
+
+    std::cout << "simulation mode: " << info2[0] << std::endl;
+
+    if(info2[0] == ".ac"){
+        n_steps = log10(string_to_float(info2[4])/(string_to_float(info2[3])))*(string_to_float(info2[2]));
+    }
+    else{
+        throw std::invalid_argument("File not compatible because simulation does not use AC analysis.");
+    }
+
+    std::vector<netvec> results;
+    std::vector<std::string> bode_results;
+
+    std::cout << "No. of steps: " << n_steps << std::endl;
+
+    for(int i = 0; i < line.size(); i++){
+        line = newline(line);
+    }
+
+    // vector 'line' only contains component now
+    for(int i = 0; i < line.size(); i++){
+           std::cout << line[i] << std::endl;
+    }
+
+    std::cout << "Get Nodes" << std::endl;
+
+    std::cout << "Line Size: " << line.size() << std::endl;
+
+   // get the number of nodes in the circuit
+   int tmp = 0; // record highest node
+
+   for(int i = 0; i < line.size(); i++){
+        std::vector<std::string> info;
+        info = Getinfo(line[i]);
+        int a, b;
+        a = label_to_idx(info[1]);
+        b = label_to_idx(info[2]);
+
+        if(a > tmp){
+            tmp = a;
+        }
+        if(b > tmp){
+            tmp = b;
+        }
+        std::cout << "tmp: "<< tmp << std::endl;
+    }
+
+    int n = tmp; // number of nodes
+    std::cout << "n: " << n << std::endl;
+
+    // if file is doing ac analysis, then continue
+    for(float i = 0; i <= n_steps; i++){
+        // find the angular frequency conditions
+        float f_n = (string_to_float(info2[3])) * pow(10, i/(string_to_float(info2[2])));
+        float w = 2* M_PI * f_n;
+
+        std::cout << "frequency: " << f_n << std::endl;
+        std::cout << "w (angular frequency): " << w << std::endl;
+
+        // building class and insert values to the matrix for each frequency
+
+        // creation of a (complex number) matrix using number of nodes
+        Eigen::MatrixXcf A_mat(n,n);
+        A_mat.setZero(); // set all matrix values to zero
+        //std::cout << A_mat << std::endl;
+
+        const std::complex<float> Re(1.0f, 0.0f); // set real part
+        const std::complex<float> Im(0.0f, 1.0f); // set imaginary part
+
+        Eigen::MatrixXcf b_vec(n,1); // creation of b matrix
+        Eigen::MatrixXcf x_vec(n,1); // creation of x matrix
+        b_vec.setZero(); // set values of b matrix to zero
+
+        for(int i = 0; i < line.size(); i++){
+            // determine the first letter
+            char firstLetter = line[i].at(0);
+            std::cout << "currently taking in: "<< firstLetter << std::endl;
+
+            std::vector<std::string> info;
+            info = Getinfo(line[i]);
+
+            // classify depending on the first letter
+            // order of code: R, C, L, D, Q, M
+
+            if(firstLetter == 'R'){
+                Resistor R(info);
+                int i = R.getNodeA();
+                int j = R.getNodeB();
+
+                if (i == 0){
+                    j--;
+                    A_mat(j,j) += R.G * Re;
+                }
+                else if(j == 0){
+                    i--;
+                    A_mat(i,i) += R.G * Re;
+                }
+                else{
+                    i--;
+                    j--;
+                    A_mat(i,i) += R.G * Re;
+                    A_mat(j,j) += R.G * Re;
+                    A_mat(i,j) -= R.G * Re;
+                    A_mat(j,i) -= R.G * Re;
+                }
+            }
+
+            if(firstLetter == 'C'){
+                Capacitor C(info);
+                C.w = w;
+                int i = C.getNodeA();
+                int j = C.getNodeB();
+
+                if (i == 0){
+                    j--;
+                    A_mat(j,j) += C.G;
+                }
+                else if(j == 0){
+                    i--;
+                    A_mat(i,i) += C.G;
+                }
+                else{
+                    i--;
+                    j--;
+                    A_mat(i,i) += C.G;
+                    A_mat(j,j) += C.G;
+                    A_mat(i,j) -= C.G;
+                    A_mat(j,i) -= C.G;
+                }
+            }
+
+            if(firstLetter == 'L'){
+                Inductor L(info);
+                L.w = w;
+                int i = L.getNodeA();
+                int j = L.getNodeB();
+
+                if (i == 0){
+                    j--;
+                    A_mat(j,j) += L.G;
+                }
+
+                else if(j == 0){
+                    i--;
+                    A_mat(i,i) += L.G;
+                }
+                else{
+                    i--;
+                    j--;
+                    A_mat(i,i) += L.G;
+                    A_mat(j,j) += L.G;
+                    A_mat(i,j) -= L.G;
+                    A_mat(j,i) -= L.G;
+                }
+            }
+
+            if(firstLetter == 'D'){
+                Diode D(info);
+                line.push_back(D.resistor_rd);
+                line.push_back(D.Id);
+            }
+
+            if(firstLetter == 'Q'){
+                BJT Q(info);
+                line.push_back(Q.resistor_rbe);
+                line.push_back(Q.resistor_r0);
+                line.push_back(Q.Ic);
+            }
+
+            if(firstLetter == 'M'){
+                MOSFET M(info);
+                line.push_back(M.resistor_rgs);
+                line.push_back(M.resistor_r0);
+                line.push_back(M.Id);
+            }
+
+            std::cout << "exiting first loop" << std::endl;
+        }
+
+        // iterate again for current source(s)
+        for(int i = 0; i < line.size(); i++){
+            std::cout << "entering second loop" << std::endl;
+
+            // this part is repeated so it needs to be simplified
+            char firstLetter = line[i].at(0);
+            std::cout << "currently taking in: " << firstLetter << std::endl;
+
+            std::vector<std::string> info;
+            info = Getinfo(line[i]);
+
+            if(firstLetter == 'I'){
+                Current_source I1(info);
+                int i = I1.getNodeA(); // in (tail of arrow)
+                int j = I1.getNodeB(); // out (head of arrow)
+
+                if(i ==0){
+                    j--;
+                    b_vec(j,0) += I1.I;
+                }
+                else if(j==0){
+                    i--;
+                    b_vec(i,0) -= I1.I;
+                }
+                else{
+                    i--;
+                    j--;
+                    b_vec(i,0) -= I1.I;
+                    b_vec(j,0) += I1.I;
+                }
+            }
+
+            if(firstLetter == 'G'){
+                V_Current_source G1(info);
+                int i = G1.getNodeA(); // in
+                int j = G1.getNodeB(); // out
+
+                if(i ==0){
+                    j--;
+                    b_vec(j,0) -= G1.I;
+                }
+                else if(j==0){
+                    i--;
+                    b_vec(i,0) += G1.I;
+                }
+                else{
+                    i--;
+                    j--;
+                    b_vec(i,0) += G1.I;
+                    b_vec(j,0) -= G1.I;
+                }
+            }
+        }
+
+        // iterate again for voltage source(s)
+        for(int i = 0; i < line.size(); i++){
+            std::cout << "entering second loop" << std::endl;
+
+            // repeated again
+            char firstLetter = line[i].at(0);
+            std::cout << "currently taking in: "<< firstLetter << std::endl;
+
+            std::vector<std::string> info;
+            info = Getinfo(line[i]);
+
+            if(firstLetter == 'V'){
+                Voltage_source V1(info);
+                int i = V1.getNodeA(); // anode
+                int j = V1.getNodeB(); // cathode
+
+                if(i ==0){
+                    j--;
+                    b_vec(j,0) = V1.V;
+
+                    for(int k = 0; k < n; k++){
+                        //changing row j to represent vj = -vsrc
+                        A_mat(j,k) = 0.0f + 0.0f*Im;
+                    }
+
+                    A_mat(j,j) = -1.0f + 0.0f*Im;
+                }
+                else if(j ==0){
+                    i--;
+                    b_vec(i,0) = V1.V;
+
+                    for(int k = 0; k < n; k++){
+                        //changing row i to represent vi = vsrc
+                        A_mat(i,k) = 0.0f + 0.0f*Im;
+                    }
+
+                    A_mat(i,i) = 1.0f + 0.0f*Im;
+                }
+
+                else{
+                    b_vec(j,0) += b_vec(i,0); //i_supernode
+                    b_vec(i,0) = V1.V; //v_src
+                    A_mat(j,j) += A_mat(i,i);
+
+
+                    for(int k = 0; k < n; k++){
+                        //changing row i to represent vi = vj + vsrc
+                        A_mat(i,k) = 0.0f + 0.0f*Im;
+                    }
+
+                    A_mat(i,i) = 1.0f + 0.0f*Im;
+                    A_mat(i,j) = -1.0f + 0.0f*Im;
+                }
+            }
+        }
+
+        std::cout << "----------------------------------------" << std::endl;
+
+        // print the A matrix and b vector (visualization purposes)
+        std::cout << std::endl << "the A matrix " << std::endl;
+        std::cout << A_mat << std::endl;
+
+        std::cout << std::endl << "the b vector" << std::endl;
+        std::cout << b_vec << std::endl;
+
+        Eigen::MatrixXcf A_inv(n,n); // creation of matrix to compute the inverse
+        A_inv = A_mat.inverse(); // compute inverse
+        std::cout << std::endl << "the inverse A matrix" << std::endl;
+        std::cout << A_inv << std::endl;
+
+        std::cout << std::endl << "test the inverse matrix (should produce identity matrix)" << std::endl;
+        std::cout << A_mat * A_inv << std::endl;
+
+        std::cout << std::endl << "the x vector (solution)" << std::endl;
+        x_vec = A_inv * b_vec;
+        std::cout << x_vec << std::endl;
+
+        std::cout << std::endl;
+
+        // store values of x vector (matrix) into results vector to save to a file
+        netvec voltage_at_nodes;
+
+        for(int i = 0; i < n; i++){
+            voltage_at_nodes.push_back(x_vec(i,0));
+        }
+
+        results.push_back(voltage_at_nodes);
+    }
+
+    std::ofstream outfile;
+    outfile.open("voltage.txt");
+
+    if(!outfile.is_open()){
+        std::cout << "error opening file" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    for(int i =0; i<results.size(); i++){
+        for(int j =0; j<results[0].size(); j++){
+            outfile << results[i][j] <<" ";
+        }
+        outfile<< "\n";
+    }
+
+    outfile.close();
+
+    // user input values for the reference node and output node
+    int n_ref, n_out;
+    std::cout << "Enter the reference node: ";
+    std::cin >> n_ref;
+    while(n_ref >= (n+1)){
+        std::cout << "Error, invalid reference node value. Enter a valid value for the reference node: ";
+        std::cin >> n_ref;
+    }
+    std::cout << "Enter the output node: ";
+    std::cin >> n_out;
+    while((n_out >= (n+1)) || (n_out == n_ref)){
+        std::cout << "Error, invalid output node value. Enter a valid value for the output node: ";
+        std::cin >> n_out;
+    }
+    n_ref = n_ref - 1;
+    n_out = n_out - 1;
+
+    std::cout << "Reference Node Values are: " << std::endl;
+    if(n_ref == -1){
+        for(int i = 0; i < results.size(); i++){
+            std::cout << "(0,0)" << std::endl;
+        }
+    }
+    else{
+        for(int i = 0; i < results.size(); i++){
+            std::cout << results[i][n_ref] << std::endl;
+        }
+    }
+
+    std::cout << std::endl << "Output Node Values are: " << std::endl;
+    for(int i = 0; i < results.size(); i++){
+        std::cout << results[i][n_out] << std::endl;
+    }
+
+    std::cout << std::endl << "Calculation of Bode Plot Entries: " << std::endl;
+    for(int i = 0; i < results.size(); i++){
+        element a = results[i][n_ref];
+        element b = results[i][n_out];
+        element c; // phasor
+
+        if(n_ref == -1){
+            // if n_ref = -1, then value is referring to ground
+            c = b;
+        }
+        else{
+            c = b / a; // transfer function = output node / reference node
+        }
+
+        float magnitude = get_magnitude(c);
+        float phase = get_phase(c);
+
+        float f_n = (string_to_float(info2[3])) * pow(10, i/(string_to_float(info2[2])));
+        float w = 2* M_PI * f_n;
+
+        // store bode plot results into bode_results in CSV format
+        std::string values;
+        values.append(std::to_string(w));
+        values.append(", ");
+        values.append(std::to_string(magnitude));
+        values.append(", ");
+        values.append(std::to_string(phase));
+
+        bode_results.push_back(values);
+    }
+
+    // print bode plot results (visualization purposes)
+    for(int i = 0; i < bode_results.size(); i++){
+        std::cout << bode_results[i] << std::endl;
+    }
+
+    std::cout << std::endl;
+
+    std::ofstream outfile2;
+    outfile2.open("bode_plot_values.txt");
+
+    if(!outfile2.is_open()){
+        std::cout << "error opening file" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    for(int i = 0; i < bode_results.size(); i++){
+        outfile2 << bode_results[i] << std::endl;
+    }
+
+    outfile2.close();
+    std::cout << "------------------------------------------------------------" << std::endl;
+    std::cout << "The AC Circuit Simulation is completed." << std::endl;
+    std::cout << "Please view the results for the bode plot in the text file." << std::endl;
+    std::cout << std::endl;
+    std::cout << "Thank you for using the AC Circuit Simulator software package." << std::endl;
+    std::cout << "Credits to Aman Narain, Christina Wang, and Yuhe Zhang" << std::endl;
+    std::cout << "------------------------------------------------------------" << std::endl << std::endl;
+}
